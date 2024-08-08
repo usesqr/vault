00b3eeb1fdac92bb2241b71b14d4c78880b074a8
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-08-08 13:10:06 -0400

    Whoops

--- consts/consts.go
+++ consts/consts.go
@@ -2,6 +2,6 @@ package consts
 
 const Brand = "Sqr"
 const ShortName = "Vault"
-const FullName = Brand + ShortName
+const FullName = Brand + " " + ShortName
 const Version = "1.0.0"
 const GitHub = "https://github.com/usesqr/vault"
