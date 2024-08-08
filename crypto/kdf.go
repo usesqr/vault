b6f177c667119d708cd59295298b00dbe59cd39b
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-08-08 16:07:32 -0400

    Add function for deriving used encryption key

--- crypto/kdf.go
+++ crypto/kdf.go
@@ -6,9 +6,15 @@ import (
 	"golang.org/x/crypto/pbkdf2"
 )
 
-func GenerateCryptoKey(password string) []byte {
+func GenerateNewCryptoKey(password string) []byte {
 	salt := GenerateSalt()
 	WriteSaltFile(salt)
 
+	return pbkdf2.Key([]byte(password), salt, 4096, 32, sha1.New)
+}
+
+func DeriveExistingCryptoKey(password string) []byte {
+	salt := ReadSaltFile()
+	
 	return pbkdf2.Key([]byte(password), salt, 4096, 32, sha1.New)
 }
