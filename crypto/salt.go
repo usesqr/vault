b4ac53a810c44ced891fa3d27eddb0e4ab407b5b
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-08-08 16:00:55 -0400

    Create salt.go

---
+++ crypto/salt.go
@@ -0,0 +1,45 @@
+package crypto
+
+import (
+	"crypto/rand"
+	"io"
+	"os"
+	"path"
+
+	"github.com/usesqr/vault/consts"
+)
+
+func GetSaltFilePath() string {
+	confDir, err := os.UserConfigDir()
+	if err != nil {
+		panic(err)
+	}
+
+	return path.Join(confDir, consts.Identifier, "salt.bin")
+}
+
+func GenerateSalt() []byte {
+	salt := make([]byte, 8)
+	_, err := io.ReadFull(rand.Reader, salt)
+	if err != nil {
+		panic(err)
+	}
+
+	return salt
+}
+
+func WriteSaltFile(salt []byte) {
+	err := os.WriteFile(GetSaltFilePath(), salt, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ReadSaltFile() []byte {
+	data, err := os.ReadFile(GetSaltFilePath())
+	if err != nil {
+		panic(err)
+	}
+
+	return data
+}
