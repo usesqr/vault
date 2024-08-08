9d6f498cec32f00b23a06159118d6f490ae5dfe5
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-08-08 15:38:18 -0400

    Use `consts.FullName` as window title

--- main.go
+++ main.go
@@ -5,13 +5,14 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"github.com/usesqr/vault/consts"
 	"github.com/usesqr/vault/ui/component"
 	"github.com/usesqr/vault/ui/dialog"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Sqr Vault")
+	w := a.NewWindow(consts.FullName)
 	w.Resize(fyne.Size{Width: 800, Height: 600})
 	w.SetMainMenu(component.CreateMainMenu(w))
 
