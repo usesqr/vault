bc2d2926b7e9da34b8ed6d48a37147f521b1048f
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-08-10 08:39:59 -0400

    Use custom theme for application

--- app/app.go
+++ app/app.go
@@ -2,10 +2,12 @@ package app
 
 import (
 	"fyne.io/fyne/v2/app"
+	"github.com/usesqr/vault/theme"
 	"github.com/usesqr/vault/ui/window"
 )
 
 func Run() {
 	a := app.New()
+	a.Settings().SetTheme(theme.SqrTheme{})
 	window.CreateMainWindow(a).ShowAndRun()
 }
