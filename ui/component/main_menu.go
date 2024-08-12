1eb94bfc76120347ae3cb1df1d6f33e1ad122689
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-08-12 04:27:27 -0400

    Add new password option to file menu

--- ui/component/main_menu.go
+++ ui/component/main_menu.go
@@ -1,17 +1,19 @@
 package component
 
 import (
-	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
 	"github.com/usesqr/vault/ui/dialog"
 )
 
 func CreateMainMenu(win fyne.Window) (menu *fyne.MainMenu) {
-	fileMenu := fyne.NewMenu("File")
+	newMenuItem := fyne.NewMenuItem("New password", func() { dialog.ShowPasswordCreateDialog(win) })
+	newMenuItem.Icon = theme.ContentAddIcon()
+	fileMenu := fyne.NewMenu("File", newMenuItem)
 
 	aboutMenuItem := fyne.NewMenuItem("About", func() { dialog.ShowAboutDialog(win) })
 	aboutMenuItem.Icon = theme.InfoIcon()
 	helpMenu := fyne.NewMenu("Help", aboutMenuItem)
-	
+
 	return fyne.NewMainMenu(fileMenu, helpMenu)
 }
