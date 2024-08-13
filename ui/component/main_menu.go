104d88552c4e271e0037d350a4b662e5f0f83311
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-08-13 09:42:33 -0400

    Add credits window

--- ui/component/main_menu.go
+++ ui/component/main_menu.go
@@ -3,6 +3,7 @@ package component
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/theme"
+	"github.com/usesqr/vault/ui/credits"
 	"github.com/usesqr/vault/ui/dialog"
 )
 
@@ -11,9 +12,13 @@ func CreateMainMenu(win fyne.Window) (menu *fyne.MainMenu) {
 	newMenuItem.Icon = theme.ContentAddIcon()
 	fileMenu := fyne.NewMenu("File", newMenuItem)
 
+	licensesMenuItem := fyne.NewMenuItem("Licenses", func() {
+		credits.CreditsWindow(fyne.CurrentApp(), fyne.NewSize(800, 400)).Show()
+	})
+	licensesMenuItem.Icon = theme.FileTextIcon()
 	aboutMenuItem := fyne.NewMenuItem("About", func() { dialog.ShowAboutDialog(win) })
 	aboutMenuItem.Icon = theme.InfoIcon()
-	helpMenu := fyne.NewMenu("Help", aboutMenuItem)
+	helpMenu := fyne.NewMenu("Help", licensesMenuItem, aboutMenuItem)
 
 	return fyne.NewMainMenu(fileMenu, helpMenu)
 }
